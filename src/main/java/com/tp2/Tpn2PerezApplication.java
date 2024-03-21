2c70213e564e82f0c8f0112439992505ecb460ea
Camilabperez <Camilabperez@hotmail.com>
2024-03-21 19:10:47 -0300

    Primer commit

---
+++ src/main/java/com/tp2/Tpn2PerezApplication.java
@@ -0,0 +1,13 @@
+package com.tp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tpn2PerezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tpn2PerezApplication.class, args);
+	}
+
+}
