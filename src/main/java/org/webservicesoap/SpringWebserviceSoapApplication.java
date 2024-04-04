437bf59dd0d5be70a8541a9674f1ac88a48769e4
Camilabperez <Camilabperez@hotmail.com>
2024-04-04 20:15:56 -0300

    WS Birthday - error "No adapter for endpoint" - ejemplo, no es del trabajo practico

--- src/main/java/org/webservicesoap/SpringWebserviceSoapApplication.java
+++ src/main/java/org/webservicesoap/SpringWebserviceSoapApplication.java
@@ -1,13 +1,13 @@
-package com.tp2;
+package org.webservicesoap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Tpn2PerezApplication {
+public class SpringWebserviceSoapApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Tpn2PerezApplication.class, args);
+		SpringApplication.run(SpringWebserviceSoapApplication.class, args);
 	}
 
 }
